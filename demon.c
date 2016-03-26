bb1e05697a3c6a81a38a9a5cdcdd813d5e11e78a
Gabriel <gabriel.mackiewicz@gmail.com>
2016-03-26 04:26:19 +0100

    dodana weryfikacja dwóch pierwszych parametrów

--- demon.c
+++ demon.c
@@ -7,55 +7,70 @@
 #include <unistd.h>
 #include <syslog.h>
 #include <string.h>
+#include <sys/stat.h>
 
-int main(void) {
-        
+int main(int argc, char * argv[]) {
+
+    struct stat sb;
+    int zrodlowy = (stat(argv[1], &sb) == 0 && S_ISDIR(sb.st_mode));
+    int docelowy = (stat(argv[2], &sb) == 0 && S_ISDIR(sb.st_mode));
+
+    if(zrodlowy && docelowy) {
+        printf("obie sciezki prowadza do katalogow\n\n");
+            exit(EXIT_SUCCESS); // zakonczenie programu przed forkowaniem - tylko do testów
+
+
         /* Our process ID and Session ID */
-        pid_t pid, sid;
-        
+            pid_t pid, sid;
+
         /* Fork off the parent process */
-        pid = fork();
-        if (pid < 0) {
+            pid = fork();
+            if (pid < 0) {
                 exit(EXIT_FAILURE);
-        }
+            }
         /* If we got a good PID, then
            we can exit the parent process. */
-        if (pid > 0) {
+            if (pid > 0) {
                 exit(EXIT_SUCCESS);
-        }
+            }
 
         /* Change the file mode mask */
-        umask(0);
-                
-        /* Open any logs here */        
-                
-        /* Create a new SID for the child process */
-        sid = setsid();
-        if (sid < 0) {
-                /* Log the failure */
-                exit(EXIT_FAILURE);
-        }
-        
+            umask(0);
 
-        
-        /* Change the current working directory */
-        if ((chdir("/")) < 0) {
+        /* Open any logs here */        
+
+        /* Create a new SID for the child process */
+            sid = setsid();
+            if (sid < 0) {
                 /* Log the failure */
                 exit(EXIT_FAILURE);
-        }
-        
+            }
+
+
+
+        /* Change the current working directory */
+            if ((chdir("/")) < 0) {
+                /* Log the failure */
+                exit(EXIT_FAILURE);
+            }
+
         /* Close out the standard file descriptors */
-        close(STDIN_FILENO);
-        close(STDOUT_FILENO);
-        close(STDERR_FILENO);
-        
+            close(STDIN_FILENO);
+            close(STDOUT_FILENO);
+            close(STDERR_FILENO);
+
         /* Daemon-specific initialization goes here */
-        
+
         /* The Big Loop */
-        while (1) {
+            while (1) {
            /* Do some task here ... */
 
            sleep(2); /* wait 30 seconds */
+            }
+            exit(EXIT_SUCCESS);
+        } else {
+            printf("ktoras sciezka okazala sie nie byc katalogiem\n");
+            printf("TO JEST KOD BLEDU ... KTORY TRZEBA EDYTOWAC\n\n");
+            exit(EXIT_FAILURE);
         }
-   exit(EXIT_SUCCESS);
-}
+    }
