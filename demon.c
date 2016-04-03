da8a360d154198d16db698402cca612354401f55
Gabriel <gabriel.mackiewicz@gmail.com>
2016-04-04 00:46:59 +0200

    dodana obsługa sygnałów
    zostało ogarnąć, dlaczego SIGUSR1 powoduje "podwójne" wybudzenie demona

--- demon.c
+++ demon.c
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <errno.h>
@@ -12,8 +13,8 @@ int rekurencyjne = 0;
 int refreshtime = 5; // 5min * 60sec = 300
 int prog_podzialu = 0;
 
-
 int logger(char* message);
+void signalhandler(int signum);
 
 int main(int argc, char * argv[]) {
 
@@ -51,7 +52,9 @@ int main(int argc, char * argv[]) {
 		printf("rek %d\n", rekurencyjne);
 		printf("reftime %d\n", refreshtime);
 		printf("prog %d\n", prog_podzialu);
-		//exit(EXIT_SUCCESS); // zakonczenie programu przed forkowaniem - tylko do testow
+
+		signal(SIGTERM, signalhandler);
+		signal(SIGUSR1, signalhandler);
 
 
 		/* Our process ID and Session ID */
@@ -73,14 +76,10 @@ int main(int argc, char * argv[]) {
 
 		/* Open any logs here */
 		setlogmask(LOG_UPTO(LOG_INFO));
-
 		openlog("SOPS1-demon", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
-
 		syslog(LOG_NOTICE, "Program uruchomiony przez uzytkownika %d", getuid());
 
-		//closelog (); // zamkniecie logow
 		//exit(EXIT_SUCCESS); // zakonczenie programu przed forkowaniem - tylko do testow
-
 
 		/* Create a new SID for the child process */
 		sid = setsid();
@@ -88,8 +87,6 @@ int main(int argc, char * argv[]) {
 			/* Log the failure */
 			exit(EXIT_FAILURE);
 		}
-
-
 
 		/* Change the current working directory */
 		if ((chdir("/")) < 0) {
@@ -107,7 +104,6 @@ int main(int argc, char * argv[]) {
 		/* The Big Loop */
 		while (1) {
 			logger("Demon zostal wybudzony automatycznie.");
-
 			// tutaj wlasciwe dzialanie demona
 
 			logger("Demon zostal uspiony.");
@@ -121,4 +117,16 @@ int main(int argc, char * argv[]) {
 
 int logger(char* message) {
 	syslog(LOG_INFO, "%s", message);
+}
+
+void signalhandler(int signum) {
+	if (signum == SIGTERM) {
+		closelog(); // zamkniecie logow
+		logger("Zakonczono dzialanie demona. [kill]");
+		exit(signum);
+	}
+	else if (signum == SIGUSR1) {
+		// wywolanie metody synchronizujacej katalogi
+		logger("Demon wybudzony przez SIGUSR1.");
+	}
 }
