e321c401a86d400d13da1fc16e0ae58baf160d0b
Gabriel <gabriel.mackiewicz@gmail.com>
2016-04-04 16:33:51 +0200

    edycje w trakcie zajec 6

--- demon.c
+++ demon.c
@@ -8,13 +8,16 @@
 #include <unistd.h>
 #include <syslog.h>
 #include <string.h>
+#include <dirent.h>
 
 int rekurencyjne = 0;
 int refreshtime = 5; // 5min * 60sec = 300
 int prog_podzialu = 0;
 
-int logger(char* message);
+void logger(char* message);
 void signalhandler(int signum);
+void listfiles (char *folder, char *path);
+int checkFile(char *plik);
 
 int main(int argc, char * argv[]) {
 
@@ -49,9 +52,9 @@ int main(int argc, char * argv[]) {
 			}
 		}
 
-		printf("rek %d\n", rekurencyjne);
-		printf("reftime %d\n", refreshtime);
-		printf("prog %d\n", prog_podzialu);
+		//printf("rek %d\n", rekurencyjne);
+		//printf("reftime %d\n", refreshtime);
+		//printf("prog %d\n", prog_podzialu);
 
 		signal(SIGTERM, signalhandler);
 		signal(SIGUSR1, signalhandler);
@@ -77,7 +80,7 @@ int main(int argc, char * argv[]) {
 		/* Open any logs here */
 		setlogmask(LOG_UPTO(LOG_INFO));
 		openlog("SOPS1-demon", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
-		syslog(LOG_NOTICE, "Program uruchomiony przez uzytkownika %d", getuid());
+		//syslog(LOG_NOTICE, "Program uruchomiony przez uzytkownika %d", getuid());
 
 		//exit(EXIT_SUCCESS); // zakonczenie programu przed forkowaniem - tylko do testow
 
@@ -88,16 +91,16 @@ int main(int argc, char * argv[]) {
 			exit(EXIT_FAILURE);
 		}
 
-		/* Change the current working directory */
-		if ((chdir("/")) < 0) {
+		/* Change the current working directory - przyda sie w rekurencji!!!!!! */
+		//if ((chdir("/")) < 0) {
 			/* Log the failure */
-			exit(EXIT_FAILURE);
-		}
+		//	exit(EXIT_FAILURE);
+		//}
 
 		/* Close out the standard file descriptors */
 		close(STDIN_FILENO);
-		close(STDOUT_FILENO);
-		close(STDERR_FILENO);
+		//close(STDOUT_FILENO);
+		//close(STDERR_FILENO);
 
 		/* Daemon-specific initialization goes here */
 
@@ -106,6 +109,7 @@ int main(int argc, char * argv[]) {
 			logger("Demon zostal wybudzony automatycznie.");
 			// tutaj wlasciwe dzialanie demona
 
+			listfiles(argv[1], argv[2]);
 			logger("Demon zostal uspiony.");
 			sleep(refreshtime); /* uspienie procesu */
 		}
@@ -115,8 +119,9 @@ int main(int argc, char * argv[]) {
 	}
 }
 
-int logger(char* message) {
-	syslog(LOG_INFO, "%s", message);
+void logger(char* message) {
+	//syslog(LOG_INFO, "%s", message);
+	fprintf(stderr, "\n%s", message);
 }
 
 void signalhandler(int signum) {
@@ -131,3 +136,87 @@ void signalhandler(int signum) {
 		sleep(refreshtime); /* uspienie procesu */
 	}
 }
+
+void listfiles (char *folder, char *path)
+{
+  DIR *dp;
+  struct dirent *ep;
+
+  dp = opendir (folder);
+  if (dp != NULL)
+    {
+      while (ep = readdir (dp))
+	    if(!strcmp(ep->d_name,".") || !strcmp(ep->d_name,".."))
+		{
+			//Katalogi specjalne, nie powinno nic robic.
+			//Można dodać pomijanie katalogów i plików ukrytych (ep-d_name[0] != '.')
+		}
+		else
+		{
+			if(ep->d_type == DT_DIR)
+			{
+				//Katalog 
+				// jeżeli rekurencja: 
+				//czy katalog istenieje w docelowym? jeżeli nie --> tworzę katalog w docelowym
+				//odpalam listfiles rekurencyjnie w katalogu (istniejącym już lub przed chwilą utworzonym)
+			}
+			else if (ep->d_type == DT_REG)
+			{
+				if(stat(ep->d_name, &file1) == 0) {
+				}
+				if ((chdir(path)) < 0) {
+					/* Log the failure */
+					exit(EXIT_FAILURE);
+				}
+				FILE *fp = fopen(ep->d_name, "rw");
+				if(!fp) {
+					logger(ep->d_name);
+					logger("brak pliku w drugim folderze\n");
+				}
+				else
+				{
+					logger(ep->d_name);
+					logger("plik jest w drugim folderze\n");
+
+					struct stat file1;
+					struct stat file2;
+					if (stat(fp->, &file2) == 0 && stat(ep->d_name, &file1) == 0) {
+						logger("stated");
+    					}
+				}
+				//logger(ep->d_name);
+				chdir("..");
+
+				if ((chdir(folder)) < 0) {
+					/* Log the failure */
+					exit(EXIT_FAILURE);
+				}
+				
+				chdir("..");
+			}
+			else
+			{
+				logger("Natrafiono na inny plik");
+			}
+		}
+      (void) closedir (dp);
+    }
+  else
+    perror ("Nie mozna otworzyc katalogu");
+}
+
+int checkFile(char * plik) {
+
+	// zwraca: 0 - pliki takie same (wazna data ostatniej modyfikacji)
+	// zwraca: 1 - pliki różne -> aktualizacja
+	// zwraca: -1 - plik nie istnieje -> tworzenie i kopia
+
+	//sprawdza czy plik istnieje w katalogu docelowym.
+	//if(istnieje) { return 1; }
+	//else{ return 0; }
+}
+
+//Funkcja sprawdzająca nadmiar w katalogu docelowym --> 
+//odpala listfiles
+//sprawdza czy któryś plik z docelowego jest w źródłowym
+//jeżeli nie, usuwa plik z docelowego
