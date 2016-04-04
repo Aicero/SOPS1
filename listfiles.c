65dda8aef39ebb8542be1372d962b550cdb09f2b
Gabriel <gabriel.mackiewicz@gmail.com>
2016-04-04 16:46:25 +0200

    edycje w trakcie zajec 6
    * edycje w trakcie zajec 6
    
    - synchronizacja postepu w pracy nad projektem

--- listfiles.c
+++ listfiles.c
@@ -6,32 +6,36 @@ Wypisywanie listy plików dostepnych w folderze podanym jako argument.
 #include <sys/types.h>
 #include <dirent.h>
 
-void listfiles (char * folder)
+void listfiles(char *folder, char *path)
 {
-  DIR *dp;
-  struct dirent *ep;
+	DIR *dp;
+	struct dirent *ep;
+	char *test;
 
-  dp = opendir (folder);
-  if (dp != NULL)
-    {
-      while (ep = readdir (dp))
-	    if(!strcmp(ep->d_name,".") || !strcmp(ep->d_name,".."))
-		{
-			//Katalogi specjalne, nie powinno nic robic. 
-		}
-		else
-		{
-			if(ep->d_type == DT_DIR)
+	dp = opendir(folder);
+	if (dp != NULL)
+	{
+		while (ep = readdir(dp))
+			if (!strcmp(ep->d_name, ".") || !strcmp(ep->d_name, ".."))
 			{
-				//Katalog
+				//Katalogi specjalne, nie powinno nic robic. 
 			}
 			else
 			{
-				//Plik
+				if (ep->d_type == DT_DIR)
+				{
+					//Katalog
+				}
+				else if (ep->d_type == DT_REG)
+				{
+					//Plik
+					test = path;
+					strcat(test, ep->d_name);
+					logger(test);
+				}
 			}
-		}
-      (void) closedir (dp);
-    }
-  else
-    perror ("Nie mozna otworzyc katalogu");
+		(void)closedir(dp);
+	}
+	else
+		perror("Nie mozna otworzyc katalogu");
 }
