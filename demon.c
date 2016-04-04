e701de579a6a91fd30b54c8aaaf13b3ea0e811f5
threydor <caoxsude@gmail.com>
2016-04-04 02:04:24 +0200

    Dodane komentarze

--- demon.c
+++ demon.c
@@ -15,6 +15,8 @@ int prog_podzialu = 0;
 
 int logger(char* message);
 void signalhandler(int signum);
+void listfiles (char * folder);
+int checkFile(char * plik){
 
 int main(int argc, char * argv[]) {
 
@@ -131,3 +133,49 @@ void signalhandler(int signum) {
 		sleep(refreshtime); /* uspienie procesu */
 	}
 }
+
+void listfiles (char * folder)
+{
+  DIR *dp;
+  struct dirent *ep;
+
+  dp = opendir (folder);
+  if (dp != NULL)
+    {
+      while (ep = readdir (dp))
+	    if(!strcmp(ep->d_name,".") || !strcmp(ep->d_name,".."))
+		{
+			//Katalogi specjalne, nie powinno nic robic.
+			//Można dodać pomijanie katalogów i plików ukrytych (ep-d_name[0] != '.')
+		}
+		else
+		{
+			if(ep->d_type == DT_DIR)
+			{
+				//Katalog 
+				// jeżeli rekurencja: 
+				//czy katalog istenieje w docelowym? jeżeli nie --> tworzę katalog w docelowym
+				//odpalam listfiles rekurencyjnie w katalogu (istniejącym już lub przed chwilą utworzonym)
+			}
+			else
+			{
+				//if(checkFile(ep->d_name)) --> jeżeli plik istnieje w katalogu docelowym, porównujemy
+				//else --> jeżeli plik nie istnieje w katalogu docelowym, kopiujemy.
+			}
+		}
+      (void) closedir (dp);
+    }
+  else
+    perror ("Nie mozna otworzyc katalogu");
+}
+
+int checkFile(char * plik){
+	//sprawdza czy plik istnieje w katalogu docelowym.
+	//if(istnieje) { return 1; }
+	//else{ return 0; }
+}
+
+//Funkcja sprawdzająca nadmiar w katalogu docelowym --> 
+//odpala listfiles
+//sprawdza czy któryś plik z docelowego jest w źródłowym
+//jeżeli nie, usuwa plik z docelowego
