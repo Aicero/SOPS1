e37b398a5fcae9051a556f296ff56baaf4cdc34d
Gabriel <gabriel.mackiewicz@gmail.com>
2016-04-11 23:35:43 +0200

    bugfix (#27)

--- combinePath.c
+++ combinePath.c
@@ -4,19 +4,19 @@ Tworzenie jednej polaczonej nazwy KATALOG + / + NAZWA_PLIKU
 
 void combinePath(char* combined, const char* katalog, const char* nazwa)
 {
-    if(katalog == NULL && nazwa == NULL) {
-        strcpy(combined, "");
-    }
-    else if(nazwa == NULL || !nazwa[0]) {
-        strcpy(combined, katalog);
-    }
-    else if(katalog == NULL || !katalog[0]) {
-        strcpy(combined, nazwa);
-    } 
-    else {
-        char directory_separator[1] = "/";
-        strcpy(combined, katalog);
+	if (katalog == NULL && nazwa == NULL) {
+		strcpy(combined, "");
+	}
+	else if (nazwa == NULL || !nazwa[0]) {
+		strcpy(combined, katalog);
+	}
+	else if (katalog == NULL || !katalog[0]) {
+		strcpy(combined, nazwa);
+	}
+	else {
+		char directory_separator[2] = "/\0";
+		strcpy(combined, katalog);
 		strcat(combined, directory_separator);
-        strcat(combined, nazwa);
-    }
+		strcat(combined, nazwa);
+	}
 }
