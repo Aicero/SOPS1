120c891ec19439e8365ec51015e6f6fe8ba15af8
Thomas <threydor@users.noreply.github.com>
2016-04-11 18:04:59 +0200

    Czyszczenie lekkie (#22)
    Zmiana daty modyfikacji w NRMcopy

--- NRMcopy.c
+++ NRMcopy.c
@@ -1,16 +1,19 @@
 /*
 Kopiowanie plików uzywajac read/write
 */
+#include <utime.h>
 
-int NRMcopy(char* pathDocelowy, mode_t modeZrodlowy) {
-
-	/* Create the file.  */
+int NRMcopy(char* pathDocelowy,time_t czasZrodlowy, mode_t modeZrodlowy) {
 	int fd = open(pathDocelowy, O_WRONLY | O_EXCL | O_CREAT, modeZrodlowy);
 	if (fd == -1) {
-		/* An error occurred. Print an error message and bail.  */
-		logger("Blad podczas otwierania pliku do zapisu.");
-		return 1;
+		/* To sie wywoluje np jezeli plik juz istnieje  */
+		
+		/* Ustawianie czasu modyfikacji pliku docelowego na czas modyfikacji pliku zrodlowego */
+		struct utimbuf nowy_czas;
+		nowy_czas.modtime = czasZrodlowy;
+		if (utime(pathDocelowy, &nowy_czas) < 0) {
+			perror(pathDocelowy);
+		}
 	}
-
 	return 0;
 }
