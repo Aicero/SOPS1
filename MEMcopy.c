ac06185fb3708b503fb5176d238d695303d9f3dc
Gabriel <gabriel.mackiewicz@gmail.com>
2016-04-13 20:31:42 +0200

    czesciowo posprzatane
    dokumentacja(add), globals(edit), demon(edit), checkfile(del),
    listfiles(edit), makefile(edit), NRMcopy(edit)

--- MEMcopy.c
+++ MEMcopy.c
@@ -3,35 +3,35 @@ Kopiowanie plików uzywajac mmap
 */
 #include <sys/mman.h>
 
-int MEMcopy(char* pathDocelowy, char* pathZrodlowy, time_t czasZrodlowy, mode_t modeZrodlowy) 
+int MEMcopy(char* pathDocelowy, char* pathZrodlowy, time_t czasZrodlowy, mode_t modeZrodlowy)
 {
-	int iZrodlowy, iDocelowy;   
-    void* source;
-    struct stat s;
-    size_t filesize;
+	int iZrodlowy, iDocelowy;
+	void* source;
+	struct stat s;
+	size_t filesize;
 
-    /* Zrodlo */
-    if(stat(pathZrodlowy, &s) != 0){
+	/* Zrodlo */
+	if (stat(pathZrodlowy, &s) != 0) {
 		/* Obsluga bledu jakby co */
 	}
-    filesize = s.st_size;
+	filesize = s.st_size;
 	iZrodlowy = open(pathZrodlowy, O_RDONLY);
-    iDocelowy = open(pathDocelowy, O_RDWR | O_CREAT, modeZrodlowy);
-    ftruncate(iDocelowy, filesize);
-	
+	iDocelowy = open(pathDocelowy, O_RDWR | O_CREAT, modeZrodlowy);
+	ftruncate(iDocelowy, filesize);
+
 	source = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE, iZrodlowy, 0);
-	if (source == MAP_FAILED){
+	if (source == MAP_FAILED) {
 		/* Obsluzyc blad */
 		perror("mmap source");
 	}
-    
-    /* Kopiowanie */
-    write(iDocelowy, source, filesize);
-	munmap(source, filesize);
-    close(iZrodlowy);
-    close(iDocelowy);
 
-    /* Ustawianie czasu modyfikacji */
+	/* Kopiowanie */
+	write(iDocelowy, source, filesize);
+	munmap(source, filesize);
+	close(iZrodlowy);
+	close(iDocelowy);
+
+	/* Ustawianie czasu modyfikacji */
 	struct utimbuf nowy_czas;
 	nowy_czas.modtime = czasZrodlowy;
 	if (utime(pathDocelowy, &nowy_czas) < 0)
--- makefile
+++ makefile
@@ -8,3 +8,20 @@ kill :
 
 sig :
 	killall -SIGUSR1 dem
+
+test :
+	./dem TESTOWANIE/zrodlo TESTOWANIE/docel -T 3 -S 2
+	
+testrek :
+	./dem TESTOWANIE/zrodlo TESTOWANIE/docel -R -T 3 -S 2
+
+clean :
+	rm -rf TESTOWANIE
+	
+prep :
+	mkdir TESTOWANIE
+	mkdir TESTOWANIE/zrodlo TESTOWANIE/docel TESTOWANIE/zrodlo/zdir1 TESTOWANIE/zrodlo/zdir2 TESTOWANIE/docel/ddir
+	touch TESTOWANIE/zrodlo/pl1 TESTOWANIE/zrodlo/pl2 TESTOWANIE/zrodlo/pl3
+	touch TESTOWANIE/docel/pliczekdousuniecia TESTOWANIE/zrodlo/zdir1/pliczekwfolderze
+	clear
+	ls -l TESTOWANIE/docel TESTOWANIE/zrodlo
