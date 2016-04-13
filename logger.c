f22469551a91796d2daca2c4d567fbdd9bd3105a
Thomas <threydor@users.noreply.github.com>
2016-04-13 20:03:46 +0200

    Current time added (#30)

--- logger.c
+++ logger.c
@@ -1,4 +1,6 @@
 void logger(char* message) {
+	time_t currentTime;
+    currentTime = time(NULL);
 	syslog(LOG_INFO, "%s", message);
-	if(verbose)fprintf(stderr, "%s", message);
+	if(verbose)fprintf(stderr, "%s %s",ctime(&currentTime), message);
 }
