c26f43ec07ad52326e42a93b9fd3e5ec7b855974
Gabriel <gabriel.mackiewicz@gmail.com>
2016-04-13 22:22:13 +0200

    nrmcopy & listfiles edited (nazwa, logger, loggerparam)

--- NRMcopy.c
+++ NRMcopy.c
@@ -4,20 +4,19 @@ Kopiowanie plików uzywajac read/write
 #include <utime.h>
 #define BUF_SIZE 8192
 
-int NRMcopy(char* pathDocelowy, char* pathZrodlowy, time_t czasZrodlowy, mode_t modeZrodlowy)
+int nrmcopy(char* pathDocelowy, char* pathZrodlowy, time_t czasZrodlowy, mode_t modeZrodlowy)
 {
 	remove(pathDocelowy);
 	int iZrodlowy, iDocelowy;
 	ssize_t inputBytes, outputBytes;    /* Number of bytes returned by read() and write() */
 	char buffer[BUF_SIZE];      /* Character buffer */
 
-								/* Tworzenie deskryptorów */
+	/* Tworzenie deskryptorów */
 	iZrodlowy = open(pathZrodlowy, O_RDONLY);
 	if (iZrodlowy == -1)
 	{
-		// logger
-		perror(pathZrodlowy);
-		fprintf(stderr, "%s\n", pathDocelowy);
+		//perror(pathZrodlowy);
+		//fprintf(stderr, "%s\n", pathDocelowy);
 		return -1;
 	}
 
@@ -25,9 +24,8 @@ int NRMcopy(char* pathDocelowy, char* pathZrodlowy, time_t czasZrodlowy, mode_t 
 	iDocelowy = open(pathDocelowy, O_WRONLY | O_TRUNC | O_CREAT, modeZrodlowy);
 	if (iDocelowy == -1)
 	{
-		// logger
-		perror(pathDocelowy);
-		fprintf(stderr, "%s\n", pathDocelowy);
+		//perror(pathDocelowy);
+		//fprintf(stderr, "%s\n", pathDocelowy);
 		return -1;
 	}
 
@@ -36,8 +34,8 @@ int NRMcopy(char* pathDocelowy, char* pathZrodlowy, time_t czasZrodlowy, mode_t 
 		outputBytes = write(iDocelowy, &buffer, (ssize_t)inputBytes);
 		if (outputBytes != inputBytes)
 		{
-			perror("Problem przy kopiowaniu pliku");
-			fprintf(stderr, "%s\n", pathDocelowy);
+			//perror("Problem przy kopiowaniu pliku");
+			//fprintf(stderr, "%s\n", pathDocelowy);
 			return -1;
 		}
 	}
@@ -51,8 +49,9 @@ int NRMcopy(char* pathDocelowy, char* pathZrodlowy, time_t czasZrodlowy, mode_t 
 	nowy_czas.modtime = czasZrodlowy;
 	if (utime(pathDocelowy, &nowy_czas) < 0)
 	{
-		// logger
-		perror(pathDocelowy);
+		//perror(pathDocelowy);
+		return -1;
 	}
+	loggerparam("Plik skopiowany do folderu docelowego.", ep->d_name);
 	return 0;
 }
