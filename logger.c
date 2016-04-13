7760c0b468159f69e491a13bae027586cbfc7e18
threydor <caoxsude@gmail.com>
2016-04-13 22:09:45 +0200

    Loggerparam added

--- logger.c
+++ logger.c
@@ -4,3 +4,9 @@ void logger(char* message) {
 	syslog(LOG_INFO, "%s", message);
 	if (g_verbose) fprintf(stderr, "%s %s", ctime(&currentTime), message);
 }
+void loggerparam(char* message, char* sParam){
+	time_t currentTime;
+	currentTime = time(NULL);
+	syslog(LOG_INFO, "%s %s", message, sParam);
+	if (g_verbose) fprintf(stderr, "%s %s %s", ctime(&currentTime), message, sParam);
+}
