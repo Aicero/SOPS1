466f97f3a090439621bbfe8326c16b010f0e864b
Gabriel <gabriel.mackiewicz@gmail.com>
2016-04-14 00:36:42 +0200

    ctime bez \n

--- logger.c
+++ logger.c
@@ -5,8 +5,11 @@ void logger(char* message) {
 	if (g_verbose) fprintf(stderr, "%s %s\n", ctime(&currentTime), message);
 }
 void loggerparam(char* message, char* sParam){
-	time_t currentTime;
-	currentTime = time(NULL);
+	time_t cTime;
+	cTime = time(NULL);
+
+	char *currentTime;
+	currentTime = strtok(ctime(&cTime), "\n");
 	syslog(LOG_INFO, "%s %s", message, sParam);
-	if (g_verbose) fprintf(stderr, "%s %s\n Parametr: %s\n\n", ctime(&currentTime), message, sParam);
+	if (g_verbose) fprintf(stderr, "%s %s\n Parametr: %s\n\n", currentTime, message, sParam);
 }
