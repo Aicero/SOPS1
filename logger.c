7446cca3aa9d9e123da08216d0032f887b0442b2
Thomas <threydor@users.noreply.github.com>
2016-04-13 23:35:00 +0200

    FIXFIX logger &  basename (#40)

--- logger.c
+++ logger.c
@@ -2,11 +2,11 @@ void logger(char* message) {
 	time_t currentTime;
 	currentTime = time(NULL);
 	syslog(LOG_INFO, "%s", message);
-	if (g_verbose) fprintf(stderr, "%s %s", ctime(&currentTime), message);
+	if (g_verbose) fprintf(stderr, "\n%s %s", ctime(&currentTime), message);
 }
 void loggerparam(char* message, char* sParam){
 	time_t currentTime;
 	currentTime = time(NULL);
 	syslog(LOG_INFO, "%s %s", message, sParam);
-	if (g_verbose) fprintf(stderr, "%s %s %s", ctime(&currentTime), message, sParam);
+	if (g_verbose) fprintf(stderr, "\n%s %s\n Parametr: %s", ctime(&currentTime), message, sParam);
 }
