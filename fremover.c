10a47132d5614b6cd1678bd3825062e69bfda471
Thomas <threydor@users.noreply.github.com>
2016-04-14 02:40:58 +0200

    Usuwanie folderow (#47)
    * Symbolic link fix & FREMOVER PROTOTYPE
    
    Ten fremover to najbardziej pojebana sprawa jaką widziałem.
    
    * Fremover, dzialajacy
    
    Nie wierze. :/

--- fremover.c
+++ fremover.c
@@ -1,15 +1,9 @@
-#define _XOPEN_SOURCE 500
-#include <string.h>
-#include <stdint.h>
 #include <ftw.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-
-
-static int unlink_cb(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
+int unlinkthis(char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
 {
     int rv = remove(fpath);
+	loggerparam("Usuwanie: ", fpath);
     if (rv)
         perror(fpath);
     return rv;
@@ -18,22 +12,12 @@ static int unlink_cb(const char *fpath, const struct stat *sb, int tflag, struct
 int rmrf(char *path)
 {
     int flags = 0;
-    flags |= FTW_DEPTH;
-    flags |= FTW_PHYS;
+    flags |= 8;
+    flags |= 2;
 
-   if (nftw(path, unlink_cb, 20, flags)
+   if (nftw(path, unlinkthis, 20, flags)
             == -1) {
         perror("nftw");
-        exit(EXIT_FAILURE);
     }
-    exit(EXIT_SUCCESS);
+    return 0;
 }
-
-int main(int argc, char *argv[]){
-	return 0;
-}
-/* 
-int rmrf(char *path)
-{
-    return nftw(path, unlink_cb, 64, FTW_DEPTH | FTW_PHYS);
-} */
