df982ce54ded5f361ec06a8c347c78ebca3c24f6
Gabriel <gabriel.mackiewicz@gmail.com>
2016-04-14 10:25:28 +0200

    Update fremover.c

--- fremover.c
+++ fremover.c
@@ -1,23 +1,23 @@
 #include <ftw.h>
-
+#define FTW_DEPTH 8
+#define FTW_PHYS 2
 int unlinkthis(char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
 {
-    int rv = remove(fpath);
+	int rv = remove(fpath);
 	loggerparam("Usuwanie: ", fpath);
-    if (rv)
-        perror(fpath);
-    return rv;
+	if (rv)
+		perror(fpath);
+	return rv;
 }
 
 int rmrf(char *path)
 {
-    int flags = 0;
-    flags |= 8;
-    flags |= 2;
+	int flags = 0;
+	flags |= FTW_DEPTH;
+	flags |= FTW_PHYS;
 
-   if (nftw(path, unlinkthis, 20, flags)
-            == -1) {
-        perror("nftw");
-    }
-    return 0;
+	if (nftw(path, unlinkthis, 20, flags) == -1) {
+		perror("nftw");
+	}
+	return 0;
 }
